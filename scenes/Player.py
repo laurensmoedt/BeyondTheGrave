1182081b61fe144266f8a9fc2b15360b69d1fc55
Laurens Moedt <laurensmoedt@gmail.com>
2020-02-26 09:20:25 +0100

    player movement to mouse pos

--- scenes/Player.py
+++ scenes/Player.py
@@ -6,6 +6,15 @@ class Player(actor):
 
     def __init__(self, win):
         self.win = win
+        self.playerImage = image('maik.png', [32, 32])
+        super().__init__(self.playerImage)
+        super().setPosition([100, 100])
+
+    def movement(self):
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                super().setPosition(pygame.mouse.get_pos())
 
     def draw(self):
         super()._draw(self.win)
+        self.movement()
--- scenes/menu.py
+++ scenes/menu.py
@@ -1,6 +1,7 @@
 import pygame
 from Extensions.color import color
 from Extensions.scenes import scenes
+from scenes.Player import Player
 
 class mainMenu:
 
@@ -9,6 +10,8 @@ class mainMenu:
         self.window = win
         # Background color init
         self.backgroundColor = self.color.custom('#70A9A1')
+        self.player = Player(self.window)
 
     def draw(self, clock, fps):
         self.window.fill(self.backgroundColor)
+        self.player.draw()
